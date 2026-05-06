d7c111f83cd015545c613f4f0af13380b2305a4b
Andy Pack <andy@sarsoo.xyz>
2026-05-06 15:30:32 +0100

    adding test cli project, commenting out dummy test, rolling back version to 0.0.1

--- TF.NET.Test/UnitTest1.cs
+++ TF.NET.Test/UnitTest1.cs
@@ -9,6 +9,6 @@ public class UnitTest1
     {
         var validate = new Validate("terraform", "/Users/andy/dev/infra/terraform/CLOUDFLARE/sas&sam");
 
-        var result = await validate.Run();
+        // var result = await validate.Run();
     }
 }
